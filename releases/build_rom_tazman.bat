32ade4a95ede64fa2c153fe37688fe4577c61ed7
Gary Shepherdson <55626312+gaz68@users.noreply.github.com>
2019-10-05 12:08:07 +0100

    Initial commit

---
+++ releases/build_rom_tazman.bat
@@ -0,0 +1,41 @@
+@echo off
+
+set    zip=tazmania.zip
+set ifiles=2c.cpu+2e.cpu+2f.cpu+2h.cpu+2j.cpu+2k.cpu+5h.cpu+5f.cpu+rom0.snd
+
+set  ofile=a.tazman.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
